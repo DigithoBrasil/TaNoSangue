0894ad6fdb9d334f7aff85f169d63f8a515317d4
Renan Siravegna <renan.siravegna@gmail.com>
2015-08-25 21:00:44 -0400

    Tentando pegar a porta default no openshift

--- teste.js
+++ teste.js
@@ -5,8 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var porta = process.env.port || 3000;
+var porta = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+var host = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-var server = app.listen(porta, function () {
-  console.log('Aberto na porta %s', porta);
+var server = app.listen(porta, host, function () {
+  console.log('Aberto em %s:%s', host, porta);
 });
